e03427aba0a172ec64beb18b9c9b0465564c36f6
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2024-02-14 14:05:28 +0530

    small edits

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://chat-webchat.netlify.app",
+    origin: 'https://chat-webchat.netlify.app',
     credentials: true
   },
 });
